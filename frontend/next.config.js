1e8474c85152679a69ec3613f8a0acfa9eb2e0db
Mohamed Aouad <draouadmohamed@gmail.com>
2025-04-14 20:09:47 +0200

    more whole structure

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['cdn.vox-cdn.com', 'platform.theverge.com'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
